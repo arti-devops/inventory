0440dcba8eba54afbf4e7431ee0670ee9b2f3f13
Al-Karid <grencisse@hotmail.com>
2024-05-10 10:12:50 +0000

    Add intial db data

---
+++ app/src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+create table ip (
+	id INT AUTO_INCREMENT,
+	ip_address VARCHAR(20),
+	assigned_to VARCHAR(50),
+	assigned_date DATE,
+	direction VARCHAR(7),
+	status VARCHAR(14)
+);
+
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('37.57.30.110', 'Isahella Armistead', '2022-07-03', 'dsesp', 'decommissioned');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('53.139.157.89', 'Red Oosthout de Vree', '2022-05-08', 'dcp', 'decommissioned');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('51.129.191.175', 'Jilleen Landy', '2022-05-02', 'daaf', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('55.229.241.56', 'Kippy Hotchkin', '2022-09-04', 'dcp', 'decommissioned');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('76.195.107.6', 'Garnette Sagerson', '2022-11-16', 'dcsti', 'decommissioned');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('191.183.120.212', 'Cheslie Maytum', '2022-02-01', 'drrn', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('176.168.131.121', 'Tabatha Capsey', '2022-12-24', 'dg', 'decommissioned');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('37.170.65.113', 'Serge Clyne', '2022-12-12', 'drrn', 'decommissioned');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('179.149.142.85', 'Alyson Sodor', '2022-05-09', 'dcp', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('24.197.215.138', 'Dean Akess', '2022-11-09', 'dcp', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('107.122.254.231', 'Annabell Zanazzi', '2022-10-08', 'dsesp', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('143.248.68.136', 'Lanny Brightie', '2022-08-29', 'dcsti', 'decommissioned');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('237.12.149.115', 'Gilemette Weagener', '2022-04-12', 'dg', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('253.240.45.173', 'Channa Whitcombe', '2022-11-23', 'dsi', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('60.29.198.35', 'Kaia Bennit', '2022-01-04', 'dgpecrp', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('73.104.113.34', 'Julina Close', '2022-09-19', 'drrn', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('234.92.120.179', 'Sada McAvin', '2022-12-23', 'dcsti', 'decommissioned');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('185.155.180.67', 'Forster Treweke', '2022-12-15', 'dsi', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('238.121.140.96', 'Zelda De la Yglesia', '2022-09-22', 'dsesp', 'active');
+insert into ip (ip_address, assigned_to, assigned_date, direction, status) values ('209.34.157.51', 'Cirillo Boxall', '2022-10-03', 'daaf', 'active');
