ce94bb30f843acf8b43f38b4e7549f7d633afbea
Al-Karid <grencisse@hotmail.com>
2024-05-13 19:51:18 +0000

    feat: Add device status column to AddressView grid

--- app/src/main/java/com/arti/inventory/ui/views/AddressView.java
+++ app/src/main/java/com/arti/inventory/ui/views/AddressView.java
@@ -7,7 +7,10 @@ import com.arti.inventory.backend.service.AddressService;
 import com.arti.inventory.ui.MainAppLayout;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.function.SerializableBiConsumer;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "/ip", layout = MainAppLayout.class)
@@ -20,11 +23,25 @@ public class AddressView extends VerticalLayout {
         add(title, paragraph);
 
         GridCrud<Address> crud = new GridCrud<>(Address.class);
-        crud.getGrid().setColumns("ipv4", "hostname", "deviceType", "connexionMode", "assignedTo", "direction", "online");
+        crud.getGrid().setColumns("ipv4", "hostname", "deviceType", "connexionMode", "assignedTo", "direction");
+        crud.getGrid().addColumn(createDeviceStatusComponentRenderer()).setHeader("Statut");
         crud.setCrudListener(addressService);
 
         setSizeFull();
         add(crud);
     }
+
+    private static final SerializableBiConsumer<Span, Address> deviceStatusComponentUpdater = (span, device) -> {
+        Boolean isOnline = device.getOnline();
+        String theme = String.format("badge %s", isOnline ? "success" : "error");
+        span.getElement().setAttribute("theme", theme);
+        span.setText(isOnline ? "En ligne" : "Hors ligne");
+    };
+
+    private static ComponentRenderer<Span, Address> createDeviceStatusComponentRenderer() {
+        return new ComponentRenderer<>(Span::new, deviceStatusComponentUpdater);
+    }
+
+
 
 }
