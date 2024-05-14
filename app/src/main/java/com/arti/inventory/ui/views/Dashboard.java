ada3b9ebdf7d289aedd0012b2a3d391437b61a9b
Al-Karid <grencisse@hotmail.com>
2024-05-14 14:42:46 +0000

    feat: Add card with computer statistics to Dashboard view

--- app/src/main/java/com/arti/inventory/ui/views/Dashboard.java
+++ app/src/main/java/com/arti/inventory/ui/views/Dashboard.java
@@ -1,7 +1,11 @@
 package com.arti.inventory.ui.views;
 
 import com.arti.inventory.ui.MainAppLayout;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -19,6 +23,27 @@ public class Dashboard extends VerticalLayout{
         add(title);
         add(new Paragraph("Le tableau de board n'est pas encore implementé."));
 
+        // CARD
+        VerticalLayout card = new VerticalLayout();
+        Div cardContent = new Div();
+        cardContent.add(new Paragraph("Ordinateurs"));
+        cardContent.add(new Hr());
+        H1 statNumber = new H1("49");
+        Anchor tableLink = new Anchor("localhost:8080", "Voir le tableau");
+        statNumber.getStyle().set("font-size", "80px");
+        statNumber.getStyle().set("text-align", "center");
+        statNumber.getStyle().set("padding", "20px 0px 20px 0px");
+        cardContent.add(statNumber);
+        cardContent.add(new Hr());
+        cardContent.add(tableLink);
+        cardContent.setSizeFull();
+        card.add(cardContent);
+
+        card.getStyle().set("box-shadow", "0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19)");
+        card.getStyle().set("border-radius", "2px");
+        card.getStyle().set("max-width", "300px");
+
+        add(card);
     }
 
 }
