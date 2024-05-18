622b0d91f517e16880fcffcefa0dc15b2832b9ba
Al-Karid <grencisse@hotmail.com>
2024-05-18 08:03:11 +0000

    feat: add sepearate configuration profiles

--- app/src/main/java/com/arti/inventory/AppCmdRunner.java
+++ app/src/main/java/com/arti/inventory/AppCmdRunner.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +19,7 @@ import com.arti.inventory.backend.repository.PhoneRepository;
 import com.arti.inventory.backend.repository.PrinterRepository;
 
 @Component
+@Profile("dev")
 public class AppCmdRunner implements CommandLineRunner{
 
     private Logger logger = org.slf4j.LoggerFactory.getLogger(AppCmdRunner.class);
