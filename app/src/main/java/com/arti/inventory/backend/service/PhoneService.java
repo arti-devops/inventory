a9a36ff5b3d2623e636cdbdaff4af5e4d968b921
Al-Karid <grencisse@hotmail.com>
2024-05-23 18:33:06 +0000

    feat: Add DeviceOnlineStatus model and update PhoneService

--- app/src/main/java/com/arti/inventory/backend/service/PhoneService.java
+++ app/src/main/java/com/arti/inventory/backend/service/PhoneService.java
@@ -3,9 +3,12 @@ package com.arti.inventory.backend.service;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.vaadin.crudui.crud.CrudListener;
 
+import com.arti.inventory.backend.model.DeviceOnlineStatus;
 import com.arti.inventory.backend.model.Phone;
 import com.arti.inventory.backend.repository.PhoneRepository;
 
@@ -20,7 +23,9 @@ public class PhoneService implements CrudListener<Phone>, DeviceService {
 
     @Override
     public Collection<Phone> findAll() {
-        return repository.findAll();
+        Collection<Phone> phones = repository.findAll();
+        phones.forEach(phone -> setDetails(phone));
+        return phones;
     }
 
     @Override
@@ -41,6 +46,17 @@ public class PhoneService implements CrudListener<Phone>, DeviceService {
     @Override
     public Integer getDeviceCount() {
         return repository.findAll().size();
+    }
+
+    public Phone setDetails(Phone phone) {
+        if (phone == null) {
+            System.err.println("Phone is null");
+            return null;
+        }
+        RestTemplate restTemplate = new RestTemplateBuilder().build();
+        DeviceOnlineStatus status = restTemplate.getForObject("http://api:3000/devices/status?ip=" + phone.getIp(), DeviceOnlineStatus.class);
+        phone.setOnline(status.status());
+        return phone;
     }
 
 }
