87c0fff2e0cbe16af7e4ed825851daa0a77617e1
Al-Karid <grencisse@hotmail.com>
2024-05-23 10:32:31 +0000

    feat: Update Dashboard to display device counts dynamically

--- app/src/main/java/com/arti/inventory/ui/views/Dashboard.java
+++ app/src/main/java/com/arti/inventory/ui/views/Dashboard.java
@@ -1,5 +1,7 @@
 package com.arti.inventory.ui.views;
 
+import com.arti.inventory.backend.service.ComputerService;
+import com.arti.inventory.backend.service.PrinterService;
 import com.arti.inventory.ui.MainAppLayout;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
@@ -19,16 +21,18 @@ public class Dashboard extends VerticalLayout{
 
     H2 title;
 
-    public Dashboard(){
+    public Dashboard(ComputerService computerService, 
+                        PrinterService printerService, 
+                        PrinterService phoneService){
 
         title = new H2("Dashboard");
         add(title);
         add(new Paragraph("Le tableau de board n'est pas encore implementé."));
 
         HorizontalLayout cardContainer = new HorizontalLayout();
-        Card card = new Card("Ordinateurs", "10");
-        Card card2 = new Card("Imprimantes", "5");
-        Card card3 = new Card("Téléphones IP", "32");
+        Card card = new Card("Ordinateurs", computerService.getDeviceCount().toString(), "computers");
+        Card card2 = new Card("Imprimantes", printerService.getDeviceCount().toString(), "printers");
+        Card card3 = new Card("Téléphones IP", phoneService.getDeviceCount().toString(), "phones");
         
         cardContainer.add(card, card2, card3);
         //cardContainer.add(card2);
@@ -43,27 +47,31 @@ public class Dashboard extends VerticalLayout{
 
     public class Card extends VerticalLayout {
     
-        public Card(String label, String stat){
+        public Card(String label, String stat, String link){
             // CARD
             Div cardContent = new Div();
+            H1 statNumber = new H1(stat);
+            Anchor tableLink = new Anchor(link, "Voir le tableau");
+            
+            // Card Strucutre
             cardContent.add(new Paragraph(label));
             cardContent.add(new Hr());
-            H1 statNumber = new H1(stat);
-            Anchor tableLink = new Anchor("localhost:8080", "Voir le tableau");
-            statNumber.getStyle().set("font-size", "80px");
-            statNumber.getStyle().set("text-align", "center");
-            statNumber.getStyle().set("padding", "20px 0px 20px 0px");
             cardContent.add(statNumber);
             cardContent.add(new Hr());
             cardContent.add(tableLink);
+            
+            // Card Style
+            statNumber.getStyle().set("font-size", "80px");
+            statNumber.getStyle().set("text-align", "center");
+            statNumber.getStyle().set("padding", "20px 0px 20px 0px");
             cardContent.setSizeFull();
-            add(cardContent);
-
+            
             getStyle().set("box-shadow", "0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.19)");
             getStyle().set("border-radius", "2px");
             getStyle().set("width", "25%");
             getStyle().set("min-width", "300px");
             
+            add(cardContent);
         }
     
     }
