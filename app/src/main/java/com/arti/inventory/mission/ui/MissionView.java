b76fc66206af70f901a4a4a01a3212ba081133c9
Al-Karid <grencisse@hotmail.com>
2024-05-30 18:19:03 +0000

    refactor: Update MissionView to display mission subject, departure date, and return date

--- app/src/main/java/com/arti/inventory/mission/ui/MissionView.java
+++ app/src/main/java/com/arti/inventory/mission/ui/MissionView.java
@@ -8,6 +8,8 @@ import com.arti.inventory.mission.backend.service.MissionService;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.shared.Tooltip;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "missions", layout = MainAppLayout.class)
@@ -21,6 +23,26 @@ public class MissionView extends VerticalLayout {
         crud.getGrid().setColumns("type", "subject", "dateOfDeparture", "dateOfReturn","location","totalBudget","status");
         crud.getGrid().getColumns().forEach(column -> column.setAutoWidth(true));
         crud.setCrudListener(service);
+
+        crud.getGrid().getColumnByKey("subject").setRenderer(new ComponentRenderer<>(mission -> {
+            Paragraph p = new Paragraph();
+            p.setText(mission.getSubject().substring(0, Math.min(mission.getSubject().length(), 20)) + "...");
+            @SuppressWarnings("unused")
+            Tooltip tooltip = Tooltip.forComponent(p).withText(mission.getSubject());
+            return p;
+        })).setHeader("Mission");
+
+        crud.getGrid().getColumnByKey("dateOfDeparture").setRenderer(new ComponentRenderer<>(mission -> {
+            Paragraph p = new Paragraph();
+            p.setText(mission.getDateOfDeparture().toString().substring(0, 10));
+            return p;
+        })).setHeader("Départ");
+
+        crud.getGrid().getColumnByKey("dateOfReturn").setRenderer(new ComponentRenderer<>(mission -> {
+            Paragraph p = new Paragraph();
+            p.setText(mission.getDateOfReturn().toString().substring(0, 10));
+            return p;
+        })).setHeader("Retour");
         
         setSizeFull();
         add(crud);
