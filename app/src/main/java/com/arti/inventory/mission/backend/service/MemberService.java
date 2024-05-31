821caaa7c6853b4b5e2c3ebf5228669e2ba9f49c
Al-Karid <grencisse@hotmail.com>
2024-05-31 15:41:14 +0000

    refactor: Implement CrudListener interface in MemberService
    This commit implements the `CrudListener` interface in the `MemberService` class. The `CrudListener` interface provides methods for performing CRUD operations on the `Member` objects. This implementation allows for easier management of member data in the backend service.

--- app/src/main/java/com/arti/inventory/mission/backend/service/MemberService.java
+++ app/src/main/java/com/arti/inventory/mission/backend/service/MemberService.java
@@ -4,18 +4,39 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.vaadin.crudui.crud.CrudListener;
 
 import com.arti.inventory.mission.backend.model.Member;
 import com.arti.inventory.mission.backend.repository.MemberRepository;
 
 @Service
-public class MemberService {
+public class MemberService implements CrudListener<Member> {
 
     @Autowired
     private MemberRepository repository;
 
     public Collection<Member> findMissionMembers(Long missionId) {
         return repository.findAllByMissionId(missionId);
+    }
+
+    @Override
+    public Collection<Member> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Member add(Member domainObjectToAdd) {
+        return repository.save(domainObjectToAdd);
+    }
+
+    @Override
+    public Member update(Member domainObjectToUpdate) {
+        return repository.save(domainObjectToUpdate);
+    }
+
+    @Override
+    public void delete(Member domainObjectToDelete) {
+        repository.delete(domainObjectToDelete);
     }
 
 }
