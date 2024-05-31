6301c5015daac4c8b002272c5b9f100967b04ed0
Al-Karid <grencisse@hotmail.com>
2024-05-31 15:41:22 +0000

    refactor: Add methods for mission status, mobility, and transportation in MissionService

--- app/src/main/java/com/arti/inventory/mission/backend/service/MissionService.java
+++ app/src/main/java/com/arti/inventory/mission/backend/service/MissionService.java
@@ -9,6 +9,9 @@ import org.vaadin.crudui.crud.CrudListener;
 import com.arti.inventory.mission.backend.model.Member;
 import com.arti.inventory.mission.backend.model.Mission;
 import com.arti.inventory.mission.backend.model.MissionType;
+import com.arti.inventory.mission.backend.model.Mobility;
+import com.arti.inventory.mission.backend.model.Status;
+import com.arti.inventory.mission.backend.model.Transportation;
 import com.arti.inventory.mission.backend.repository.MissionRepository;
 
 @Service
@@ -69,5 +72,56 @@ public class MissionService implements CrudListener<Mission> {
         Collection<Member> members = memberService.findMissionMembers(mission.getId());
         mission.setTotalBudget(members.stream().mapToLong(Member::getTotalBudget).sum());
         mission.setMembers(members);
+    }
+
+    public static String getMissionStatus(Status status) {
+        if (status.equals(Status.PENDING)) {
+            return String.valueOf("EN ATTENTE");
+        } else if (status.equals(Status.APPROVED)) {
+            return String.valueOf("APPROUVÉE");
+        } else if (status.equals(Status.REJECTED)) {
+            return String.valueOf("REJETÉE");
+        }
+        return String.valueOf("INCONNU");
+    }
+
+    public static String getMissionStatusTheme(Status status) {
+        if (status.equals(Status.PENDING)) {
+            return "badge tertiary";
+        } else if (status.equals(Status.APPROVED)) {
+            return "badge primary success";
+        } else if (status.equals(Status.REJECTED)) {
+            return "badge primary error";
+        }
+        return "badge contrast tertiary";
+    }
+
+    public static String getMobility(Mobility mobility) {
+        if (mobility.equals(Mobility.PERSONAL_CAR)) {
+            return String.valueOf("Voiture personnelle");
+        } else if (mobility.equals(Mobility.COMPANY_CAR)) {
+            return String.valueOf("Voiture de service");
+        } else if (mobility.equals(Mobility.PUBLIC_TRANSPORT)) {
+            return String.valueOf("Transport en commun");
+        } else if (mobility.equals(Mobility.OTHER)) {
+            return String.valueOf("Autre");
+        }
+        return String.valueOf("Inconnu");
+    }
+
+    public static String getTransportation(Transportation transportation) {
+        if (transportation.equals(Transportation.CAR)) {
+            return String.valueOf("Voiture");
+        } else if (transportation.equals(Transportation.TRAIN)) {
+            return String.valueOf("Train");
+        } else if (transportation.equals(Transportation.PLANE)) {
+            return String.valueOf("Avion");
+        } else if (transportation.equals(Transportation.BOAT)) {
+            return String.valueOf("Bateau");
+        } else if (transportation.equals(Transportation.OTHER)) {
+            return String.valueOf("Autre");
+        }
+        return String.valueOf("Inconnu");
+
     }
 }
